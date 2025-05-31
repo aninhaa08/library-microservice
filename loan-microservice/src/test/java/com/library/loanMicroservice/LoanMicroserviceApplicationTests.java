2513ea0d1e9c2fa9ecb3b78c9c0a1e38d943fa3e
Ana Carolina <anacarolinamb2@gmail.com>
2025-05-31 19:13:13 -0300

    feat-adding-springboot-base

---
+++ loan-microservice/src/test/java/com/library/loanMicroservice/LoanMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.loanMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
