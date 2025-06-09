a5476fbffa90e1e27b2aa317ecd638fafd77f3a6
Lívia Lana <livialivialana1106@gmail.com>
2025-06-09 20:57:02 -0300

    Adicionando as rotas de gênero

--- loan-microservice/src/main/java/com/library/loanMicroservice/service/GenreService.java
+++ loan-microservice/src/main/java/com/library/loanMicroservice/service/GenreService.java
@@ -1,12 +1,21 @@
 package com.library.loanMicroservice.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.library.loanMicroservice.model.Genre;
 import com.library.loanMicroservice.repository.GenreRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -22,4 +31,5 @@ public class GenreService {
         }
         return "Gênero excluído.";
     }
+
 }
